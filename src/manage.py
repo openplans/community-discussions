5e59871c654fb6f7fa34431545a246dc7c6cd6f1
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-01-28 16:58:57 -0500

    Include pybbm fork as a submodule

--- src/manage.py
+++ src/manage.py
@@ -7,6 +7,7 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
+    sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '../lib/pybbm/'))
     sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..'))
 
     from django.core.management import execute_from_command_line
