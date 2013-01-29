6c6b5423739777551c315eb77b3633ae1d47ea99
Aaron Ogle <atogle@gmail.com>
2013-01-29 15:28:01 -0500

    Add GeoDjango to installed apps

--- src/settings.py
+++ src/settings.py
@@ -64,6 +64,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.admin',
+    'django.contrib.gis',
     'pybb',
     'pytils',
     'registration',
