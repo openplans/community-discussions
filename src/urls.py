58836da93030c720bfd95835c6cdbd2849d12d9d
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-02-14 12:12:15 -0500

    Allow adding a topic from the watch area page
    - Add New Topic button to the watch area page
    - Override the watch area topics view to incorporate the global forum pk
    - Override the add post view to accept a watch area to start in. If specified, the form will be initialized with the place at the centroid of the watch area's fence

--- src/urls.py
+++ src/urls.py
@@ -3,13 +3,16 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from views import ForumView
+from views import ForumView, WatchAreaTopicsView, AddPostView
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
-    # Override the index view in pybb
+    # Intercept the following views from pybb
     url('^forum/$', ForumView.as_view(), name='global_forum'),
+    url('^watch_area/(?P<pk>\d+)/$', WatchAreaTopicsView.as_view(), name='watch_area_topics'),
+    url('^forum/(?P<forum_id>\d+)/topic/add/$', AddPostView.as_view(), name='add_topic'),
+
     (r'^', include('pybb.urls', namespace='pybb')),
     (r'^accounts/', include('registration.urls')),
 
--- src/views.py
+++ src/views.py
@@ -1,11 +1,46 @@
 from django.conf import settings
+from django.shortcuts import get_object_or_404
 from pybb.views import ForumView as PybbForumView
+from pybb.views import WatchAreaTopicsView as PybbWatchAreaTopicsView
+from pybb.views import AddPostView as PybbAddPostView
+from pybb.models import WatchArea
 
 
-class ForumView (PybbForumView):
-
-    def dispatch(self, *args, **kwargs):
+class GlobalForumMixin (object):
+    def get_forum_pk(self):
         if not hasattr(settings, 'PYBB_FORUM_ID'):
             raise ImproperlyConfigured('You must provide a PYBB_FORUM_ID setting')
+        return settings.PYBB_FORUM_ID
+
+
+class ForumView (GlobalForumMixin, PybbForumView):
+
+    def dispatch(self, *args, **kwargs):
+        forum_pk = self.get_forum_pk()
         return super(ForumView, self).dispatch(
-            pk=settings.PYBB_FORUM_ID, *args, **kwargs)
+            pk=forum_pk, *args, **kwargs)
+
+
+class WatchAreaTopicsView (GlobalForumMixin, PybbWatchAreaTopicsView):
+
+    def get_context_data(self, **kwargs):
+        forum_pk = self.get_forum_pk()
+        context = super(WatchAreaTopicsView, self).get_context_data(**kwargs)
+        context['forum'] = {'id': forum_pk}
+        return context
+
+
+class AddPostView (PybbAddPostView):
+
+    def get_initial_watch_area(self):
+        pk = self.request.GET.get('start_in_watch_area', None)
+        if pk is not None:
+            watch_area = get_object_or_404(WatchArea, pk=pk)
+            return watch_area
+
+    def get_form_kwargs(self):
+        kwargs = super(AddPostView, self).get_form_kwargs()
+        watch_area = self.get_initial_watch_area()
+        if watch_area is not None:
+            kwargs['initial']['place'] = watch_area.fence.centroid
+        return kwargs
