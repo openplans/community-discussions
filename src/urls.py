156783aefc30b3e247f9275f56fa92d786e4c21a
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-02-14 14:00:02 -0500

    Improve breadcrumbs
    - Update the reference to pybbm so that watch areas trace back to the global forum when a PYBB_FORUM_ID is provided
    - Fix the text for adding/editing watch areas on various templates
    - Remove the now unneeded override for WatchAreaTopicsList

--- src/urls.py
+++ src/urls.py
@@ -3,14 +3,13 @@ from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-from views import ForumView, WatchAreaTopicsView, AddPostView
+from views import ForumView, AddPostView
 
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
 
     # Intercept the following views from pybb
     url('^forum/$', ForumView.as_view(), name='global_forum'),
-    url('^watch_area/(?P<pk>\d+)/$', WatchAreaTopicsView.as_view(), name='watch_area_topics'),
     url('^forum/(?P<forum_id>\d+)/topic/add/$', AddPostView.as_view(), name='add_topic'),
 
     (r'^', include('pybb.urls', namespace='pybb')),
--- src/views.py
+++ src/views.py
@@ -1,33 +1,20 @@
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 from pybb.views import ForumView as PybbForumView
-from pybb.views import WatchAreaTopicsView as PybbWatchAreaTopicsView
 from pybb.views import AddPostView as PybbAddPostView
 from pybb.models import WatchArea
 
 
-class GlobalForumMixin (object):
+class ForumView (PybbForumView):
     def get_forum_pk(self):
         if not hasattr(settings, 'PYBB_FORUM_ID'):
             raise ImproperlyConfigured('You must provide a PYBB_FORUM_ID setting')
         return settings.PYBB_FORUM_ID
 
-
-class ForumView (GlobalForumMixin, PybbForumView):
-
     def dispatch(self, *args, **kwargs):
         forum_pk = self.get_forum_pk()
         return super(ForumView, self).dispatch(
             pk=forum_pk, *args, **kwargs)
-
-
-class WatchAreaTopicsView (GlobalForumMixin, PybbWatchAreaTopicsView):
-
-    def get_context_data(self, **kwargs):
-        forum_pk = self.get_forum_pk()
-        context = super(WatchAreaTopicsView, self).get_context_data(**kwargs)
-        context['forum'] = {'id': forum_pk}
-        return context
 
 
 class AddPostView (PybbAddPostView):
