0a773f3ccf5c48fbe7fc9c0debe4bbb3368c633a
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-03-09 12:17:36 -0500

    Scroll the list item corresponding to a shape into view on click
    - Wrap the initialization code in a jQuery ready block to ensure that the page elements (list items) are available to bind to
    - Only move the list item if it is not already visible in the scroll area

--- src/static/js/homepage-map.js
+++ src/static/js/homepage-map.js
@@ -1,6 +1,6 @@
 var CommunityDiscussions = CommunityDiscussions || {};
 
-(function(CD) {
+(function(CD, $) {
   CD.initHomepageMap = function(options) {
     var map = new L.Map(options.el, {
           scrollWheelZoom: false
@@ -13,10 +13,14 @@ var CommunityDiscussions = CommunityDiscussions || {};
       var $listItem = $(shape.watchArea.selector),
           $list = $listItem.parent(),
           itemOffset = $listItem.position().top,
-          listOffset = $list.scrollTop();
+          itemHeight = $listItem.height(),
+          listOffset = $list.scrollTop(),
+          listHeight = $list.height();
 
-      // Move the list item to the top
-      $list.animate({scrollTop: itemOffset + listOffset});
+      // Move the list item to the top, if it's not all visible
+      if (itemOffset < 0 || (itemOffset+itemHeight) > listHeight) {
+        $list.animate({scrollTop: itemOffset + listOffset});
+      }
 
       // Designate the item as active
       $('.is-active').removeClass('is-active');
@@ -26,42 +30,47 @@ var CommunityDiscussions = CommunityDiscussions || {};
       map.fitBounds(shape.getBounds());
     };
 
-    // Set the view to the default center in case there are no places
-    map.setView(options.center, 14).addLayer(layer);
+    $(function() {
+      // Set the view to the default center in case there are no places
+      map.setView(options.center, 14).addLayer(layer);
 
-    for (i=0, len=options.watchAreas.length; i<len; i++) {
-      watchArea = options.watchAreas[i];
+      for (i=0, len=options.watchAreas.length; i<len; i++) {
+        watchArea = options.watchAreas[i];
 
-      // Construct the fence's shape and bind events
-      shape = L.geoJson(watchArea.fence);
-      shape.watchArea = watchArea;
+        // Construct the fence's shape and bind events
+        shape = L.geoJson(watchArea.fence);
+        shape.watchArea = watchArea;
 
-      shape.on('click', function(e) {
-        activateWatchAreaLayer(this);
-      });
-//      $(watchArea.selector).click(function(e) {
-//        e.preventDefault();
-//        activateWatchAreaLayer(this);
-//      });
+        shape.on('click', function(e) {
+          activateWatchAreaLayer(this);
+        });
 
-      // Add the shape to the feature group
-      featureGroup.addLayer(shape);
-    }
+        $(watchArea.selector).data('shape', shape);
+        $(watchArea.selector).click(function(e) {
+          e.preventDefault();
+          activateWatchAreaLayer($(this).data('shape'));
+        });
 
-    for(i=0, len=options.topics.length; i<len; i++) {
-      topic = options.topics[i];
+        // Add the shape to the feature group
+        featureGroup.addLayer(shape);
+      }
 
-      // Construct and bind events to the topic marker
-      marker = L.marker([topic.place.y, topic.place.x], { icon: options.icon });
-      marker.bindPopup('<a href="'+topic.url+'">'+topic.name+'</a>');
 
-      // Add to the feature group (already on the map)
-      featureGroup.addLayer(marker);
-    }
+      for(i=0, len=options.topics.length; i<len; i++) {
+        topic = options.topics[i];
 
-    if (len) {
-      // Fit the map to all the features
-      map.fitBounds(featureGroup.getBounds());
-    }
+        // Construct and bind events to the topic marker
+        marker = L.marker([topic.place.y, topic.place.x], { icon: options.icon });
+        marker.bindPopup('<a href="'+topic.url+'">'+topic.name+'</a>');
+
+        // Add to the feature group (already on the map)
+        featureGroup.addLayer(marker);
+      }
+
+      if (len) {
+        // Fit the map to all the features
+        map.fitBounds(featureGroup.getBounds());
+      }
+    });
   };
-})(CommunityDiscussions);
+})(CommunityDiscussions, jQuery);
