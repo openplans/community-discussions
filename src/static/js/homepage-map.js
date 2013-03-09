a62c5f453b39818254308a030198bf9c820f1032
Mjumbe Wawatu Ukweli <mjumbewu@gmail.com>
2013-03-09 12:33:44 -0500

    Highlight shapes on the map when activated
    - Add active and inactive styles
    - Create a new feature group for shapes, apart from markers so that inactive styles can just be applied to that entire group

--- src/static/js/homepage-map.js
+++ src/static/js/homepage-map.js
@@ -6,7 +6,24 @@ var CommunityDiscussions = CommunityDiscussions || {};
           scrollWheelZoom: false
         }),
         layer = new L.TileLayer(options.tileUrl, {maxZoom: 17, attribution: options.tileAttribution}),
+        watchAreaGroup = L.featureGroup().addTo(map),
         featureGroup = L.featureGroup().addTo(map),
+
+        // TODO: This style is used in several places. Should be added as a
+        //       global configuration.
+        shapeActiveStyle = {
+          color: '#478e23',
+          weight: 2,
+          opacity: 1,
+          fillOpacity: 0.3
+        },
+        shapeInactiveStyle = {
+          color: '#888',
+          weight: 2,
+          opacity: 1,
+          fillOpacity: 0.3
+        },
+
         topic, watchArea, itemSelector, i, len, marker;
 
     var activateWatchAreaLayer = function(shape) {
@@ -24,7 +41,10 @@ var CommunityDiscussions = CommunityDiscussions || {};
 
       // Designate the item as active
       $('.is-active').removeClass('is-active');
+      watchAreaGroup.setStyle(shapeInactiveStyle);
+
       $listItem.addClass('is-active');
+      shape.setStyle(shapeActiveStyle);
 
       // Zoom the map to the shape
       map.fitBounds(shape.getBounds());
@@ -38,7 +58,7 @@ var CommunityDiscussions = CommunityDiscussions || {};
         watchArea = options.watchAreas[i];
 
         // Construct the fence's shape and bind events
-        shape = L.geoJson(watchArea.fence);
+        shape = L.geoJson(watchArea.fence, {style: shapeInactiveStyle});
         shape.watchArea = watchArea;
 
         shape.on('click', function(e) {
@@ -51,8 +71,8 @@ var CommunityDiscussions = CommunityDiscussions || {};
           activateWatchAreaLayer($(this).data('shape'));
         });
 
-        // Add the shape to the feature group
-        featureGroup.addLayer(shape);
+        // Add the shape to the watch area group
+        watchAreaGroup.addLayer(shape);
       }
 
 
@@ -69,7 +89,7 @@ var CommunityDiscussions = CommunityDiscussions || {};
 
       if (len) {
         // Fit the map to all the features
-        map.fitBounds(featureGroup.getBounds());
+        map.fitBounds(featureGroup.getBounds().extend(watchAreaGroup.getBounds()));
       }
     });
   };
